45a5bb1d29e1cfab495aacb582c859b3d7da38f2
ordinaka <victorordinaka@gmail.com>
2026-04-20 11:46:09 +0100

    Fix: Add cloudinary config and update upload middleware

---
+++ backend/src/config/cloudinary.ts
@@ -0,0 +1,11 @@
+import { v2 as cloudinary } from "cloudinary";
+import { env } from "./env";
+
+cloudinary.config({
+  cloud_name: env.cloudinaryCloudName,
+  api_key: env.cloudinaryApiKey,
+  api_secret: env.cloudinaryApiSecret,
+  secure: true
+});
+
+export { cloudinary };
